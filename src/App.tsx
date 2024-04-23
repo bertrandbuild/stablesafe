e9951f81c0cedd1b414ec3ee0ff7b0b8610f7b4e
Bertrand <bertrandrigal.pro@gmail.com>
2024-04-23 12:30:01 +0200

    Add UI input

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { IExecDataProtector } from "@iexec/dataprotector";
 
 const ADMIN_USER_ADDRESS = "0x754edfB906252B304f89c59c61f4368028bdcE6c";
 const WEB3MAIL_APP_ADDRESS = "0x781482C39CcE25546583EaC4957Fb7Bf04C277D2";
+const EXPLORER_URL = "https://explorer.iex.ec/bellecour/dataset/";
 
 const getProvider = () => {
   const web3Provider = window.ethereum;
@@ -17,13 +18,19 @@ const getProvider = () => {
 
 async function initDataProtector() {
   const web3Provider = getProvider();
-  const dataProtector = new IExecDataProtector(web3Provider);
+  return new IExecDataProtector(web3Provider);
+}
+
+// Signup : Protect email and grant access to web3mail app
+async function signup(email: string): Promise<string> {
+  console.log("Signing up :", email);
+  const dataProtector = await initDataProtector();
   if (!dataProtector) {
     throw new Error("Data protector is not available.");
   }
 
   try {
-    const protectedData = await dataProtector.protectData({ data: { "test@mail.com" } });
+    const protectedData = await dataProtector.protectData({ data: { email } });
     console.log("Protected Data:", protectedData);
 
     const grantedAccess = await dataProtector.grantAccess({
@@ -41,10 +48,55 @@ async function initDataProtector() {
   }
 }
 
+function SignUpComponent() {
+  const [email, setEmail] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const [resultTxHash, setResultTxHash] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  const handleSignUp = async () => {
+    try {
+      setLoading(true);
+      const signupResult = await signup(email);
+      setResultTxHash(signupResult);
+      setLoading(false);
+    } catch (error: unknown) {
+      setLoading(false);
+      if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        throw new Error(
+          "An unexpected error occurred during the signup process."
+        );
+      }
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Enter your email"
+      />
+      <button onClick={handleSignUp}>Sign Up</button>
+      {loading && <p>loading</p>}
+      {error && <p>Error: {error}</p>}
+      {resultTxHash && (
+        <div>
+          <h4>Private Signup Successful</h4>
+          <p>
+            You can check the transaction proof on the {" "}
+            <a target="_blank" href={EXPLORER_URL + resultTxHash}>iExec chain explorer</a>
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
+
 function App() {
-
-  initDataProtector();
-
   return (
     <>
       <div>
@@ -56,13 +108,12 @@ function App() {
         </a>
       </div>
       <h1>Depeg Alert</h1>
-      <div className="card">
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <SignUpComponent />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
