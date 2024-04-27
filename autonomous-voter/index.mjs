b2fbb1380c52d7e8a0794b00b8f3f01de4a21944
Bertrand <bertrandrigal.pro@gmail.com>
2024-04-27 21:09:57 +0200

    Add vote to tableland if depeg

--- autonomous-voter/index.mjs
+++ autonomous-voter/index.mjs
@@ -1,9 +1,11 @@
 import { IExecOracleManage } from './services/iExecOracle.mjs';
 import { IExecWeb3mailManager } from './services/iExecWeb3mail.mjs';
+import { TableLandManager } from './services/TableLand.mjs';
 
 const ADMIN_PROTECT_EMAIL = "0xfb85516C8344E803745479D8c834C759F2dE3b52";
 const mailManager = new IExecWeb3mailManager("bellecour");
 const oracleUsdc = new IExecOracleManage("bellecour", "usd-coin", "usd");
+const table = new TableLandManager();
 
 export const update = async () => {
   oracleUsdc.updateOracle();
@@ -16,8 +18,10 @@ export const run = async (event, context) => {
     const { value: newUsdcValue } = await oracleUsdc.readOracle();
     if (newUsdcValue < 0.994) {
       console.log('Depeg detected');
+      // Send email to other voters to double check
       mailManager.sendEmail(ADMIN_PROTECT_EMAIL);
-      // email.sendEmailToAllContacts();
+      // Vote to update the notation
+      table.insertNotation(3, `a depeg has been detected at: ${time}, usdec value: ${newUsdcValue}`); 
     }
     console.log(`New usdc value: ${newUsdcValue}`);
   } catch (error) {
