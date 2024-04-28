7caa3e07feba832e09d7dce5b75fb3518604a0b1
Bertrand <bertrandrigal.pro@gmail.com>
2024-04-28 09:09:01 +0200

    Add router and 404 page

---
+++ src/Page404.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function Page404() {
+  return (
+    <>
+      <div className="header">
+        <h1>404 - not found</h1>
+      </div>
+    </>
+  );
+}
+
+export default Page404;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App.tsx";
+import Page404 from "./Page404.tsx";
+import Admin from "./Admin.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Page404 />,
+  },
+  {
+    path: "/admin",
+    element: <Admin />,
+    errorElement: <Page404 />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
